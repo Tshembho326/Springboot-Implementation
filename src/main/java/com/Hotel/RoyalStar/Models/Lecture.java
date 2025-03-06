e71006a8fe170c897771e2195ef86d3ff2417c62
Tshembho326 <xvmrhu001@myuct.ac.za>
2025-03-06 18:27:30 +0200

    Created the lecture entity

--- src/main/java/com/Hotel/RoyalStar/Models/Lecture.java
+++ src/main/java/com/Hotel/RoyalStar/Models/Lecture.java
@@ -1,4 +1,56 @@
 package com.Hotel.RoyalStar.Models;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table
 public class Lecture {
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "lecture_sequence"
+    )
+    @SequenceGenerator(
+            name = "lecture_sequence",
+            sequenceName = "lecture_sequence",
+            allocationSize = 1
+    )
+    private Integer id;
+
+    private String name;
+
+    public Lecture() {}
+
+    public Lecture(String name) {
+        this.name = name;
+    }
+
+    public Lecture(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Lecture{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
