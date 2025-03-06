daacbbad67ccc34b1941e5147845104320883b46
Tshembho326 <xvmrhu001@myuct.ac.za>
2025-03-06 18:35:04 +0200

    Added repositories for all the modelled entities

---
+++ src/main/java/com/Hotel/RoyalStar/Repositories/LectureRepository.java
@@ -0,0 +1,7 @@
+package com.Hotel.RoyalStar.Repositories;
+
+import com.Hotel.RoyalStar.Models.Lecture;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LectureRepository extends JpaRepository<Lecture, Integer> {
+}
---
+++ src/main/java/com/Hotel/RoyalStar/Repositories/ResourceRepository.java
@@ -0,0 +1,7 @@
+package com.Hotel.RoyalStar.Repositories;
+
+import com.Hotel.RoyalStar.Models.Resource;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ResourceRepository extends JpaRepository<Resource, Integer> {
+}
