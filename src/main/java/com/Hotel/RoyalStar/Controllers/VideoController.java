0e4662e65d1df61d56de2b0b4bfab5cc613288f5
Tshembho326 <xvmrhu001@myuct.ac.za>
2025-03-09 22:29:09 +0200

    Added the Video Controller

--- src/main/java/com/Hotel/RoyalStar/Controllers/VideoController.java
+++ src/main/java/com/Hotel/RoyalStar/Controllers/VideoController.java
@@ -1,4 +1,58 @@
 package com.Hotel.RoyalStar.Controllers;
 
+import com.Hotel.RoyalStar.Models.Video;
+import com.Hotel.RoyalStar.Services.VideoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/videos/")
 public class VideoController {
+
+    private final VideoService videoService;
+
+    @Autowired
+    public VideoController(VideoService videoService) {
+        this.videoService = videoService;
+    }
+
+    @GetMapping
+    public List<Video> getAllVideos() {
+        return videoService.getAllVideos();
+    }
+
+    @GetMapping("{video_id}")
+    public Video getVideo(
+            @PathVariable("video_id") Integer videoId
+    ) {
+        return videoService.getVideo(videoId);
+    }
+
+    @PostMapping
+    public void addVideo(
+            @RequestBody Video video
+    ) {
+        videoService.addVideo(video);
+    }
+
+    @DeleteMapping("{video_id}")
+    public void deleteVideo(
+            @PathVariable("video_id") Integer videoId
+    ) {
+        videoService.deleteVideo(videoId);
+    }
+
+    @PutMapping("{video_id}")
+    public void updateVideo(
+            @PathVariable("video_id") Integer videoId,
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) Integer size,
+            @RequestParam(required = false) String url,
+            @RequestParam(required = false) Integer length
+    ) {
+        videoService.updateVideo(videoId, name, size, url, length);
+    }
 }
+
--- src/main/java/com/Hotel/RoyalStar/Services/VideoService.java
+++ src/main/java/com/Hotel/RoyalStar/Services/VideoService.java
@@ -1,4 +1,50 @@
 package com.Hotel.RoyalStar.Services;
 
+import com.Hotel.RoyalStar.Models.Video;
+import com.Hotel.RoyalStar.Repositories.VideoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class VideoService {
+
+    private final VideoRepository videoRepository;
+
+    @Autowired
+    public VideoService(VideoRepository videoRepository) {
+        this.videoRepository = videoRepository;
+    }
+
+    public List<Video> getAllVideos() {
+        return videoRepository.findAll();
+    }
+
+    public Video getVideo(Integer videoId) {
+        return videoRepository.findById(videoId).orElseThrow(
+                () -> new IllegalArgumentException("Video not found with ID: " + videoId)
+        );
+    }
+
+    public void addVideo(Video video) {
+        videoRepository.save(video);
+    }
+
+    public void deleteVideo(Integer videoId) {
+        boolean validation = videoRepository.existsById(videoId);
+        if (!validation) videoRepository.deleteById(videoId);
+    }
+
+    public void updateVideo(Integer videoId, String name, Integer size, String url, Integer length) {
+        Video video = videoRepository.findById(videoId).orElseThrow(
+                () -> new IllegalArgumentException("Video not found with ID: " + videoId)
+        );
+        if (name != null) video.setName(name);
+        if (size != null) video.setSize(size);
+        if (url != null) video.setUrl(url);
+        if (length != null) video.setLength(length);
+
+        videoRepository.save(video);
+    }
 }
