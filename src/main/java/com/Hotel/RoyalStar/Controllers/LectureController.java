6ab243bc25fdf39ccc70e571ace540bb7426561d
Tshembho326 <xvmrhu001@myuct.ac.za>
2025-03-09 21:06:04 +0200

    Added the Lecture Controller

--- src/main/java/com/Hotel/RoyalStar/Controllers/LectureController.java
+++ src/main/java/com/Hotel/RoyalStar/Controllers/LectureController.java
@@ -1,4 +1,54 @@
 package com.Hotel.RoyalStar.Controllers;
 
+import com.Hotel.RoyalStar.Services.LectureService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.Hotel.RoyalStar.Models.Lecture;
+import java.util.List;
+
+@RestController
+@RequestMapping("api/lectures/")
 public class LectureController {
+
+    private final LectureService lectureService;
+
+    @Autowired
+    public LectureController(LectureService lectureService) {
+        this.lectureService = lectureService;
+    }
+
+    @GetMapping
+    public List<Lecture> getAllLectures() {
+        return lectureService.getAllLectures();
+    }
+
+    @GetMapping("{lecture_id}")
+    public Lecture getLecture(
+            @PathVariable("lecture_id")Integer lectureId
+    ) {
+        return lectureService.getLecture(lectureId);
+    }
+
+    @PostMapping
+    public void addLecture(
+            @RequestBody Lecture lecture
+    ) {
+        lectureService.addLecture(lecture);
+    }
+
+    @DeleteMapping("{lecture_id}")
+    public void deleteLecture(
+            @PathVariable("lecture_id") Integer lectureId
+    ) {
+        lectureService.deleteLecture(lectureId);
+    }
+
+    @PutMapping("{lecture_id}")
+    public void updateLecture(
+            @PathVariable("lecture_id") Integer lectureId,
+            @RequestParam(required = false) String name
+    ) {
+        lectureService.updateLecture(lectureId, name);
+    }
 }
--- src/main/java/com/Hotel/RoyalStar/Services/LectureService.java
+++ src/main/java/com/Hotel/RoyalStar/Services/LectureService.java
@@ -1,4 +1,22 @@
 package com.Hotel.RoyalStar.Services;
 
+import com.Hotel.RoyalStar.Models.Lecture;
+
+import java.util.List;
+
 public class LectureService {
+    public List<Lecture> getAllLectures() {
+    }
+
+    public Lecture getLecture(Integer lectureId) {
+    }
+
+    public void addLecture(Lecture lecture) {
+    }
+
+    public void deleteLecture(Integer lectureId) {
+    }
+
+    public void updateLecture(Integer lectureId, String name) {
+    }
 }
